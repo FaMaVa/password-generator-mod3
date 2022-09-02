c6c458fad982d000ce3c99495c4988313ce17f19
Faith Vaz <faith.m.vaz@gmail.com>
2022-09-01 19:46:16 -0600

    finished code in JS and started README

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
-# password-generator-mod3
+# Random Password Generator
+
+## Description
+
+This webpage
+
+## Table of Contents
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [Credits](#credits)
+- [License](#license)
+
+## Installation
+
+N/A
+
+## Usage
+
+Link to webpage: 
+<br/> "deployed link"
+<br/>
+<br/>To use the webpage, 
+
+![alt text](assets/03-javascript-homework-demo.png)
+
+## Credits
+
+Starter code provided by:
+<br/>Xandromus at https://github.com/Xandromus
+<br/>GitHub repository: https://github.com/coding-boot-camp/friendly-parakeet.git
+
+## License
+
+N/A
--- script.js
+++ script.js
@@ -1,14 +1,80 @@
-// Assignment Code
+// Assignment code here
+var characters = [];
+
+// Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+  var charChoice = prompt("How many characters would you like your password to contain?");
+  characterChoice = parseInt(charChoice, 10)
+  if (!characterChoice) {
+    return;
+  };
+  if (characterChoice >= 8 && characterChoice <= 128) {
+    var amount = characterChoice;
+    console.log(amount);
+  } else if (characterChoice < 8) {
+    alert("Not enough characters!");
+    return;
+  } else {
+    alert("Too many characters!");
+    return;
+  };
+
+  var specialChoice = confirm("Click OK to confirm including special characters.");
+  if (specialChoice === true) {
+    characters.push(" ", "!", "\"", "#", "$", "%", "&", "\'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "\\", "]", "^", "_", "`", "{", "|", "}", "~");
+    console.log("has special characters.");
+  } else {
+    console.log("no special characters.");
+  };
+
+  var numericChoice = confirm("Click OK to confirm numeric numeric characters.");
+  if (numericChoice === true) {
+    characters.push("1", "2", "3", "4", "5", "6", "7", "8", "9", "0");
+    console.log("has numbers.");
+  } else {
+    console.log("no numbers.");
+  };
+
+  var lowercaseChoice = confirm("Click OK to confirm including lowercase characters.");
+  if (lowercaseChoice === true) {
+    characters.push("a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z");
+    console.log("has lowercase letters.");
+  } else {
+    console.log("no lowercase letters.");
+  };
+
+  var uppercaseChoice = confirm("Click OK to confirm including uppercase characters.");
+  if (uppercaseChoice === true) {
+    characters.push("A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z");
+    console.log("has uppercase letters.")
+  } else {
+    console.log("no uppercase letters.");
+  };
+
+  function generatePassword(length) {        // found how to at: https://openjavascript.info/2022/03/14 random-password-generator-using-javascript/
+    let password = '';
+    let passwordLength = length;
+   
+    const array = new Uint32Array(length); // Create 'unsigned' array
+    window.crypto.getRandomValues(array); // Assign random values to new array
+   
+    for (let i = 0; i < passwordLength; i++) {
+      password += characters[array[i] % characters.length]; // % operator returns remainder of division
+    }
+   
+    const msg = `Your new password is: "${password}"`
+    console.log(msg);
+    return password;
+  }
+  var password = generatePassword(amount);
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 
 }
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
